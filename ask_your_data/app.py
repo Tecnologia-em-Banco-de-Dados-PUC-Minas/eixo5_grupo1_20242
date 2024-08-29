20cb613878e520478fe2b5f56525d2601bee7d28
Edvaldo Gutierres <edvaldo_gutierres@yahoo.com.br>
2024-08-29 00:18:23 -0300

    fix: Ajuste app.py - criando modulações

--- ask_your_data/app.py
+++ ask_your_data/app.py
@@ -6,165 +6,203 @@ from process_langchain.retrieval_qa import create_retrieval_qa
 from utils.text_processing import get_chunks
 from utils.extract_data_pdf import extract_text_from_pdf
 from qdrant_client import QdrantClient
-
 from dotenv import load_dotenv
 import os
 
-load_dotenv(".env")
-qdrant_api_key = os.environ.get("OPENAI_API_KEY")
-qdrant_url = os.environ.get("QDRANT_URL")
-qdrant_api_key = os.environ.get("QDRANT_API_KEY")
 
-st.set_page_config(page_title="Pergunte ao seu PDF!", page_icon=":books:")
+# Carrega variáveis de ambiente
+def load_env_variables():
+    load_dotenv(".env")
+    qdrant_api_key = os.environ.get("OPENAI_API_KEY")
+    qdrant_url = os.environ.get("QDRANT_URL")
+    qdrant_api_key = os.environ.get("QDRANT_API_KEY")
+    return qdrant_url, qdrant_api_key
 
 
 # Inicializações apenas uma vez, evitando recarregamento em cada interação
 @st.cache_resource
-def init_components(local=True):
+def init_components(local=True, qdrant_url=None, qdrant_api_key=None):
     if local:
-        # Criação da coleção Qdrant Local
         client = QdrantClient(host="localhost", port=6333)
         create_collection("openai_collection", 1536)
     else:
-        # Criação da coleção Qdrant Cloud
         client = QdrantClient(url=qdrant_url, api_key=qdrant_api_key)
         create_collection_cloud("openai_collection", 1536)
 
-    # Inicialização do processo de embedding
     vectorstore = initialize_embeddings("openai_collection", client)
-
-    # Armazenando o vectorstore no estado da sessão
     st.session_state.vectorstore = vectorstore
     st.session_state.qa = create_retrieval_qa(vectorstore)
-
     return vectorstore, st.session_state.qa
 
 
-# Botão para selecionar entre Qdrant Local e Cloud
-use_cloud = st.sidebar.checkbox("Usar Qdrant Cloud")
+# Processa os arquivos PDF carregados e salva o texto extraído
+def process_uploaded_files(uploaded_files, save_path):
+    txt_file_path = None  # Inicializa a variável
+    for uploaded_file in uploaded_files:
+        if uploaded_file is not None:
+            file_path = os.path.join(save_path, uploaded_file.name)
+            with open(file_path, "wb") as f:
+                f.write(uploaded_file.getbuffer())
 
-# Verifique se o vectorstore já foi inicializado
-if "vectorstore" not in st.session_state:
-    vectorstore, qa = init_components(local=not use_cloud)
-else:
-    vectorstore = st.session_state.vectorstore
-    qa = st.session_state.qa
+            txt_file_name = uploaded_file.name.replace(".pdf", "") + ".txt"
+            txt_file_path = os.path.join("assets/txt/", txt_file_name)
 
-# Título da barra lateral
-st.sidebar.title("Bem vindo ao AskYourPDF")
+            try:
+                extract_text_from_pdf(file_path, txt_file_path)
+                st.sidebar.write(f"Arquivo salvo com sucesso!")
+            except Exception as e:
+                st.sidebar.write(f"Erro ao processar o arquivo: {e}")
 
-# File uploader na barra lateral
-uploaded_files = st.sidebar.file_uploader(
-    "Escolha um novo arquivo para analisar.", type=["pdf"], accept_multiple_files=True
-)
+    return txt_file_path
 
-# Caminho da pasta onde os arquivos serão salvos
-save_path = "assets/pdf"  # Substitua pelo caminho desejado
 
-# Verifica se a pasta existe, se não, cria a pasta
-if not os.path.exists(save_path):
-    os.makedirs(save_path)
+# Executa o processamento dos dados
+def process_data(txt_file_path, use_cloud, qdrant_url, qdrant_api_key):
+    st.info("Iniciando o processamento...")
 
-# Processa cada arquivo carregado
-for uploaded_file in uploaded_files:
-    if uploaded_file is not None:
-        # Constrói o caminho completo para salvar o arquivo
-        file_path = os.path.join(save_path, uploaded_file.name)
+    progress_bar = st.progress(0)
+    progress_status = st.empty()
 
-        # Salva o arquivo na pasta especificada
-        with open(file_path, "wb") as f:
-            f.write(uploaded_file.getbuffer())
+    client = QdrantClient(
+        host="localhost" if not use_cloud else qdrant_url,
+        port=6333 if not use_cloud else None,
+        api_key=None if not use_cloud else qdrant_api_key,
+    )
+    st.success("Cliente Qdrant criado com sucesso.")
+    progress_bar.progress(25)
+    progress_status.text("Cliente Qdrant criado.")
 
-        # Define o nome do arquivo .txt
-        txt_file_name = uploaded_file.name.replace(".pdf", "") + ".txt"
-        txt_file_path = os.path.join("assets/txt/", txt_file_name)
+    if use_cloud:
+        st.info("Usando Qdrant Cloud")
+        create_collection_cloud("openai_collection", 1536)
+    else:
+        st.info("Usando Qdrant Local")
+        create_collection("openai_collection", 1536)
 
-        # Chama a função para extrair texto do PDF e salvá-lo em um arquivo .txt
-        try:
-            result = extract_text_from_pdf(file_path, txt_file_path)
-            st.sidebar.write(f"Arquivo salvo com sucesso!")
-        except Exception as e:
-            st.sidebar.write(f"Erro ao processar o arquivo: {e}")
+    progress_bar.progress(50)
+    progress_status.text("Coleção criada com sucesso.")
 
-st.header("Converse com o seu PDF")
+    vectorstore = initialize_embeddings("openai_collection", client)
+    st.success("Vectorstore inicializado com sucesso.")
+    progress_bar.progress(75)
+    progress_status.text("Vectorstore inicializado.")
 
-with st.sidebar:
+    with open(txt_file_path, encoding="utf-8") as f:
+        raw_text = f.read()
 
-    if st.button("Processar dados"):
-        st.info("Iniciando o processamento...")
+    texts = get_chunks(raw_text)
+    st.success("Textos carregados com sucesso.")
+    progress_bar.progress(90)
+    progress_status.text("Textos carregados.")
 
-        # Criação da barra de progresso
-        progress_bar = st.progress(0)
-        progress_status = st.empty()
+    vectorstore.add_texts(texts)
+    st.success("Textos adicionados ao vectorstore com sucesso!")
+    progress_bar.progress(100)
+    progress_status.text("Processamento concluído!")
 
-        # Passo 1: Criação da coleção Qdrant
-        client = QdrantClient(
-            host="localhost" if not use_cloud else qdrant_url,
-            port=6333 if not use_cloud else None,
-            api_key=None if not use_cloud else qdrant_api_key,
-        )
-        st.success("Cliente Qdrant criado com sucesso.")
-        progress_bar.progress(25)
-        progress_status.text("Cliente Qdrant criado.")
+    st.success("Processo concluído com sucesso!!!")
+    return vectorstore
 
-        if use_cloud:
-            st.info("Usando Qdrant Cloud")
-            create_collection_cloud("openai_collection", 1536)
-        else:
-            st.info("Usando Qdrant Local")
-            create_collection("openai_collection", 1536)
 
-        progress_bar.progress(50)
-        progress_status.text("Coleção criada com sucesso.")
+# Inicializa o histórico de conversação
+def initialize_conversation_history():
+    if "historico_conversa" not in st.session_state:
+        st.session_state.historico_conversa = []
 
-        # Passo 2: Inicialização do processo de embedding
-        vectorstore = initialize_embeddings("openai_collection", client)
-        st.success("Vectorstore inicializado com sucesso.")
-        progress_bar.progress(75)
-        progress_status.text("Vectorstore inicializado.")
 
-        # Passo 3: Carregando e processando o texto
-        with open(txt_file_path, encoding="utf-8") as f:
-            raw_text = f.read()
-
-        texts = get_chunks(raw_text)
-        st.success("Textos carregados com sucesso.")
-        progress_bar.progress(90)
-        progress_status.text("Textos carregados.")
-
-        # Passo 4: Adicionando textos ao banco de dados
-        vectorstore.add_texts(texts)
-        st.success("Textos adicionados ao vectorstore com sucesso!")
-        progress_bar.progress(100)
-        progress_status.text("Processamento concluído!")
-
-        st.success("Processo concluído com sucesso!!!")
-
-# Inicializa o histórico de conversação se ainda não existir
-if "historico_conversa" not in st.session_state:
-    st.session_state.historico_conversa = []
-
-# Campo de entrada para a pergunta
-user_input = st.text_input("Digite sua pergunta:", key="input")
-
-# Ao pressionar Enter ou clicar no botão Enviar
-if st.button("Enviar"):
-    # Verifica se o input não está vazio
+# Processa a pergunta do usuário e obtém a resposta
+def process_user_input(user_input, qa):
     if user_input:
-        # Adiciona a pergunta do usuário ao histórico
         st.session_state.historico_conversa.append(
             {"message": user_input, "is_user": True}
         )
-
-        # Verifica se vectorstore não é None
-        if vectorstore is not None:
-            response = qa.run(user_input)
-
-        # Adiciona a resposta ao histórico de conversação
+        response = qa.run(user_input)
         st.session_state.historico_conversa.append(
             {"message": response, "is_user": False}
         )
 
-    # Exibe o histórico de conversação
     for i, chat in enumerate(st.session_state.historico_conversa):
         message(chat["message"], is_user=chat["is_user"], key=f"msg_{i}")
+
+
+# Configura a página inicial
+def configure_page():
+    st.set_page_config(
+        page_title="Pergunte ao seu PDF!",
+        page_icon=":books:",
+        # layout="wide",
+        initial_sidebar_state="expanded",
+    )
+
+
+# Função de login
+def login():
+    st.title("Login")
+    username = st.text_input("Usuário")
+    password = st.text_input("Senha", type="password")
+    if st.button("Entrar"):
+        if (
+            username == "admin" and password == "admin"
+        ):  # Substitua por um método de autenticação real
+            st.session_state["logged_in"] = True
+            st.query_params["logged_in"] = "True"
+        else:
+            st.error("Usuário ou senha incorretos")
+
+
+# Função principal
+def main():
+    configure_page()
+
+    if "logged_in" not in st.session_state:
+        st.session_state["logged_in"] = False
+
+    if not st.session_state["logged_in"]:
+        login()
+    else:
+        qdrant_url, qdrant_api_key = load_env_variables()
+
+        use_cloud = st.sidebar.checkbox("Usar Qdrant Cloud")
+
+        if "vectorstore" not in st.session_state:
+            vectorstore, qa = init_components(
+                local=not use_cloud,
+                qdrant_url=qdrant_url,
+                qdrant_api_key=qdrant_api_key,
+            )
+        else:
+            vectorstore = st.session_state.vectorstore
+            qa = st.session_state.qa
+
+        st.sidebar.title("Bem vindo ao AskYourPDF")
+
+        uploaded_files = st.sidebar.file_uploader(
+            "Escolha um novo arquivo para analisar.",
+            type=["pdf"],
+            accept_multiple_files=True,
+        )
+
+        save_path = "assets/pdf"
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        txt_file_path = process_uploaded_files(uploaded_files, save_path)
+
+        if st.sidebar.button("Processar dados"):
+            if txt_file_path:  # Verifica se um arquivo foi processado
+                process_data(txt_file_path, use_cloud, qdrant_url, qdrant_api_key)
+            else:
+                st.warning("Nenhum arquivo foi carregado ou processado.")
+
+        st.header("Converse com o seu PDF")
+
+        initialize_conversation_history()
+
+        user_input = st.text_input("Digite sua pergunta:", key="input")
+
+        if st.button("Enviar"):
+            process_user_input(user_input, qa)
+
+
+if __name__ == "__main__":
+    main()
